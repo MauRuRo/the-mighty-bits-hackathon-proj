e98ff9d126944c18820700496c7644038684fa39
MauRuRo <m.rubenderoos@gmail.com>
2021-03-06 10:14:10 +0000

    setup gmaps

--- static/js/main.js
+++ static/js/main.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
     
 })
 
-//Initialise map function
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        disableDefaultUI: true,
-        center: {
-            lat: 53.35001680243868, //Dublin
-            lng: -6.262019153232362
-        }
-    });
-};
+// //Initialise map function
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         disableDefaultUI: true,
+//         center: {
+//             lat: 53.35001680243868, //Dublin
+//             lng: -6.262019153232362
+//         }
+//     });
+// };
