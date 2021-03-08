cf168ecd867fc04b8e4fe8824e95f2ea90b3c43e
MauRuRo <m.rubenderoos@gmail.com>
2021-03-08 08:18:34 +0000

    Fixed typo 'is' to 'in' in settings.py line 29

--- paddys_trivia/settings.py
+++ paddys_trivia/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVLOPMENT' is os.environ
+DEBUG = 'DEVLOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['paddys-quiz.herokuapp.com', 'localhost']
 
